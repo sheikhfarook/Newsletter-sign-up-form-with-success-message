1e715d560ce9efaa783eb608cb66bf742ec33f0d
Farook <mohammedfarook085@gmail.com>
2023-12-31 13:33:32 +0530

    removed chakra ui

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { CharkraProvider } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CharkraProvider>
-      <App />
-    </CharkraProvider>
+    <App />
   </React.StrictMode>
 );
